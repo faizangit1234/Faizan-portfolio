ed74ed19547f9774a6cacc5e66778bfe55437fa2
faizangit1234 <163971794+faizangit1234@users.noreply.github.com>
2024-07-10 20:15:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+<script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js">
+
+
+
+
+
+    function linkedin(){
+        window.open("https://www.linkedin.com/in/faizan-farooq-7110952ab/")
+    }
+    function github(){
+        window.open("https://github.com/faizangit1234/Faizan-portfolio")
+    }
+
+
+
+
+</script>
